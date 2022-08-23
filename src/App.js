dbb23448c380b727d2a96dfa88cc70d6e8fc462b
carlos.jarquin <carlos.jarquin@sss-tech.com.tw>
2022-08-23 17:43:53 +0800

    first update

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import "./index.css";
+
+function App() {
+  return (
+
+    <div>
+        <ul>Hello</ul>
+        <ul>Hello</ul>
+        <ul>Hello</ul>
+        <ul>Hello</ul>
+    </div>
+  )
+}
+
+export default App;
